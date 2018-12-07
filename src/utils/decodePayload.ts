e7a7d35b62d819e03371b17bf7733dbc11a88bd3
Andrew Wei <andrewscwei@gmail.com>
2018-12-07 12:04:51 -0800

    Remove BSON support

--- src/utils/decodePayload.ts
+++ src/utils/decodePayload.ts
@@ -1,46 +1,7 @@
-import is from '@sindresorhus/is';
-import { ObjectID } from 'bson';
 import { MessagePayload } from '../types';
-
-function mapValuesToObjectID(obj: { [key: string]: any }) {
-  for (const k in obj) {
-    if (!obj.hasOwnProperty(k)) continue;
-
-    const val = obj[k];
-
-    if (is.array(val)) {
-      obj[k] = val.map(v => mapValuesToObjectID(v));
-    }
-    else if (is.plainObject(val)) {
-      obj[k] = mapValuesToObjectID(val);
-    }
-    else if (isValidObjectID(val)) {
-      obj[k] = new ObjectID(val);
-    }
-  }
-
-  return obj;
-}
-
-function isValidObjectID(val: any) {
-  try {
-    if (val instanceof ObjectID) return true;
-    if (!ObjectID.isValid(val)) return false;
-
-    const objectId = new ObjectID(val);
-
-    if (objectId.toHexString() !== String(val)) return false;
-
-    return true;
-  }
-  catch (err) {
-    return false;
-  }
-}
 
 export default function decodePayload(buffer: Buffer): MessagePayload {
   const str = buffer.toString();
   const obj = JSON.parse(str);
-
-  return mapValuesToObjectID(obj);
+  return obj;
 }
--- src/utils/encodePayload.ts
+++ src/utils/encodePayload.ts
@@ -1,29 +1,6 @@
-import is from '@sindresorhus/is';
-import { ObjectID } from 'bson';
 import { MessagePayload } from '../types';
 
-function mapObjectIDsToString(obj: { [key: string]: any }) {
-  for (const k in obj) {
-    if (!obj.hasOwnProperty(k)) continue;
-
-    const val = obj[k];
-
-    if (is.array(val)) {
-      obj[k] = val.map(v => mapObjectIDsToString(v));
-    }
-    else if (is.plainObject(val)) {
-      obj[k] = mapObjectIDsToString(val);
-    }
-    else if (val instanceof ObjectID) {
-      obj[k] = val.toHexString();
-    }
-  }
-
-  return obj;
-}
-
 export default function encodePayload(payload: MessagePayload): Buffer {
-  const obj = mapObjectIDsToString(payload);
-  const str = JSON.stringify(obj);
+  const str = JSON.stringify(payload);
   return Buffer.from(str);
 }
