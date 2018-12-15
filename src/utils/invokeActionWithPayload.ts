a71745ea5e6d177b2fd28f9f8bff7340f435878c
Andrew Wei <andrewscwei@gmail.com>
2018-12-14 23:09:05 -0800

    Make parser optional

--- src/utils/invokeActionWithPayload.ts
+++ src/utils/invokeActionWithPayload.ts
@@ -1,12 +1,12 @@
 import { ActionWithParams, MessagePayload, MessagePayloadMake } from '../types';
 
-export default function invokeActionWithPayload<T extends { [key: string]: any } = {}>(action: ActionWithParams<T>, parser: (payload: MessagePayload) => T | Promise<T>) {
+export default function invokeActionWithPayload<T extends { [key: string]: any } = {}>(action: ActionWithParams<T>, parser?: (payload: MessagePayload) => T | Promise<T>) {
   return async (payload: MessagePayload) => {
     const { data, error } = payload;
 
     if (error) throw new Error(error.message);
 
-    const params = await parser({ data });
+    const params = parser ? await parser({ data }) : { ...data };
     const res = await action(params);
     return MessagePayloadMake(res);
   };
